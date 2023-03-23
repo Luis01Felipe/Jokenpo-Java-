fe0f455bbc5eb1766308fc2689bb61659e06df9c
Beater <104606257+Luis01Felipe@users.noreply.github.com>
2023-03-23 20:29:30 -0300

    Add files via upload

---
+++ Jokenpo/src/Main.java
@@ -0,0 +1,72 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner pC = new Scanner(System.in);
+
+        int playerChoice;
+        int computerChoice;
+        int playerScore   = 0;
+        int computerScore = 0;
+
+        do {
+            do {
+                System.out.println("Escolha entre: \n1. Pedra, 2. Papel, 3. Tesoura");
+
+                while (!pC.hasNextInt()) {
+                    System.out.println("Formato invalido, por favor tente novamente.");
+                    pC.next();
+                }
+
+                playerChoice = pC.nextInt();
+
+                switch (playerChoice) {
+                    case 1:
+                        System.out.println("Você escolheu *Pedra*");
+                        break;
+                    case 2:
+                        System.out.println("Você escolheu *Papel*");
+                        break;
+                    case 3:
+                        System.out.println("Você escolheu *Tesoura*");
+                        break;
+                    default:
+                        System.out.println("Escolha incorreta, por favor tente novamente.");
+                        break;
+                }
+            } while (playerChoice < 1 || playerChoice > 3);
+
+            Random cC = new Random();
+            do {
+                computerChoice = cC.nextInt(3) + 1;
+            } while (computerChoice == playerChoice);
+
+            switch (computerChoice) {
+                case 1:
+                    System.out.println("Computador escolheu *Pedra*");
+                    break;
+                case 2:
+                    System.out.println("Computador escolheu *Papel*");
+                    break;
+                case 3:
+                    System.out.println("Computador escolheu *Tesoura*");
+                    break;
+            }
+
+            if (computerChoice == 1 && playerChoice == 2 || computerChoice == 2 && playerChoice == 3 || computerChoice == 3 && playerChoice == 1) {
+                System.out.println("Jogador ganhou");
+                playerScore+=1;
+            } else {
+                //if (computerChoice == 2 && playerChoice == 1 || computerChoice == 3 && playerChoice == 2 || computerChoice == 1 && playerChoice == 3) {
+                System.out.println("Computador ganhou");
+                computerScore+=1;
+            }
+        }while(playerScore < 3 && computerScore < 3);
+
+        System.out.println("\n[Jogador]: "+playerScore+"\n[Computador]: "+computerScore);
+
+        pC.close();
+    }
+}
+
